03718fa71b1b2f4512aaed4d9faeaca4a0d347fe
khaled404 <khaaleed.maahmmoud@gmail.com>
2019-09-08 16:56:29 +0200

    App V4

--- main.js
+++ main.js
@@ -177,24 +177,23 @@ if(body.classList.contains('main')){
     fbBtn.addEventListener('click', (e) =>{
         e.preventDefault();
 
-        auth.signInWithPopup(provider)
-        // auth.signInWithPopup(provider).then((result) => {
-        //     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-        //     var token = result.credential.accessToken;
+        auth.signInWithPopup(provider).then((result) => {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var token = result.credential.accessToken;
             
-        //     // The signed-in user info.
-        //     var user = result.user;
-        //     // ...
-        //   }).catch(function(error) {
-        //     // Handle Errors here.
-        //     var errorCode = error.code;
-        //     var errorMessage = error.message;
-        //     // The email of the user's account used.
-        //     var email = error.email;
-        //     // The firebase.auth.AuthCredential type that was used.
-        //     var credential = error.credential;
-        //     // ...
-        //   });
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+          }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+          });
           
 
 
